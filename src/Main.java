3973940d49d160d445c1683f5492eff4bda84b0a
Tommy <57922367+tommy128works@users.noreply.github.com>
2025-06-04 18:30:14 -0400

    Print menu

---
+++ src/Main.java
@@ -0,0 +1,8 @@
+public class Main {
+    public static void main(String[] args) {
+
+        TextUI textUI = new TextUI();
+        textUI.printMenu();
+
+    }
+}
---
+++ src/TextUI.java
@@ -0,0 +1,20 @@
+public class TextUI {
+
+    public TextUI() {
+
+
+    }
+
+
+
+    public void printMenu() {
+        System.out.println("Welcome to the traffic management system!");
+        System.out.println("Menu:");
+        System.out.println("1. Add");
+        System.out.println("2. Delete");
+        System.out.println("3. System");
+        System.out.println("0. Quit");
+    }
+
+
+}
